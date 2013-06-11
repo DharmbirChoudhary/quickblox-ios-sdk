bb7f882035b27237b1ecf76800449069a870e6c0
Kirill Ushkov <kirill.ushkov@injoit.com>
2013-06-11 16:01:43 +0300

    Fixes in app delegate

--- sample-custom-objects/SimpleSample-custom-object-ios/AppDelegate.h
+++ sample-custom-objects/SimpleSample-custom-object-ios/AppDelegate.h
@@ -14,4 +14,6 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (strong, nonatomic) UIWindow *window;
+
 @end
--- sample-custom-objects/SimpleSample-custom-object-ios/AppDelegate.m
+++ sample-custom-objects/SimpleSample-custom-object-ios/AppDelegate.m
@@ -7,8 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "SplashViewController.h"
-#import "MainViewController.h"
 
 @implementation AppDelegate
 
